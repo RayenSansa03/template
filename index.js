1421a6bd4bb256c8b1315784c38ec35df4845e0f
RayenSansa03 <mohamedrayen.sansa@esprit.tn>
2024-06-15 21:31:40 +0100

    test

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://rsansa204:kHXQpt2VuBf79d0x@cluster0.u5twbj9.mongodb.net/Node?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Connection failed", error);
+  });
